0e2cf0602e306e661b1ac1d216160899fd7b059c
Val Spiridonov et al <valery.spiridonov@gmail.com>
2019-12-06 13:24:24 -0500

    task comment

---
+++ chess_piece_destinations.py
@@ -0,0 +1,9 @@
+'''
+The Task:
+
+Accept two parameters:
+1.	Type of chess piece (Queen, Rook, Knight)
+2.	Current position on a chess board (for example: d2)
+Return:
+ A list of all the potential board positions the given piece could advance to, with one move, from the given position, with the assumption there are no other pieces on the board.
+'''
